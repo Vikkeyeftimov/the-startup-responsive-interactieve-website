ca4c76fdd90732b48cf5c69acfca4b23482db39b
Vikkeyeftimov <Vikksaus@hotmail.com>
2024-01-16 22:02:42 +0100

    fd

--- fundascript.js
+++ fundascript.js
@@ -1,7 +1,18 @@
-const buttons = document.querySelectorAll("[data-slider-button]")
+// const buttons = document.querySelectorAll("[data-carousel-button]")
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        const pffset = button.dataset.sliderButton === "next" ? 1 : -1
-    })
-} )
+// buttons.forEach(button => {
+//     button.addEventListener("click", () => {
+//         const offset = button.dataset.carouselButton === "tweede" ? 1 : -1
+//         const slides = button
+//         .closest("[data-slider]")
+//         .querySelector("[data-slids]")
+
+//     const activeSlide = slides.querySelector("[data-active]")  
+//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
+//     if (newIndex < 0) newIndex = slides.children.length - 1 
+//     if (newindex >= slides.children.length) newIndex = 0 
+
+//     slides.children[newIndex].dataset.active = true
+//     delete activeSlide.dataset.active
+//     })
+// } ) 
