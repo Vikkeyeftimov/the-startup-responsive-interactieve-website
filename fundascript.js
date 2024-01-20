f0db72fc55a077a604e4684aa90dc0f83f90ef85
Vikkeyeftimov <Vikksaus@hotmail.com>
2024-01-20 21:39:38 +0100

    nieuwe js

--- fundascript.js
+++ fundascript.js
@@ -1,27 +1,6 @@
-// const buttons = document.querySelectorAll("[data-carousel-button]")
+var plusbutton = document.querySelector(".plusbutton");
+var extratext = document.querySelector(".vergroten");
 
-// buttons.forEach(button => {
-//     button.addEventListener("click", () => {
-//         const offset = button.dataset.carouselButton === "tweede" ? 1 : -1
-//         const slides = button
-//         .closest("[data-slider]")
-//         .querySelector("[data-slids]")
-
-//     const activeSlide = slides.querySelector("[data-active]")  
-//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-//     if (newIndex < 0) newIndex = slides.children.length - 1 
-//     if (newindex >= slides.children.length) newIndex = 0 
-
-//     slides.children[newIndex].dataset.active = true
-//     delete activeSlide.dataset.active
-//     })
-// } ) 
-
-let button = document.querySelector(".volledig")
-let begin = document.querySelector("start")
-let eind = document.querySelector("hele")
-
-button,addEventListener('click', ad)
-function change(){
-    begin
-}
+plusbutton.addEventListener("click", function() {
+    element.classList.toggle("yay");
+});
