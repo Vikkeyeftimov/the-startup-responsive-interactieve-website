888a6f937ea42985359a8955c9b2d97ec35edd06
Vikkeyeftimov <Vikksaus@hotmail.com>
2024-01-22 22:51:46 +0100

    nieuwste

--- fundascript.js
+++ fundascript.js
@@ -1,6 +1,15 @@
-var plusbutton = document.querySelector(".plusbutton");
-var extratext = document.querySelector(".vergroten");
 
-plusbutton.addEventListener("click", function() {
-    element.classList.toggle("yay");
-});
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .image-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+
+    slideButtons.forEach( button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1; 
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+        });
+    });
+}
+
+window.addEventListener("load", initSlider)
